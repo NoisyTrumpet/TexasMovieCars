8b77e220a2880e1f496f55c21728fd288146cfbb
Cesar Benavides <cesar@Cesars-MacBook-Pro.local>
2019-10-30 15:49:15 -0500

    Fonts and most of header

--- app/Controllers/App.php
+++ app/Controllers/App.php
@@ -35,7 +35,10 @@ class App extends Controller {
 	public function primarymenu() {
 		$args = array(
 			'theme_location' => 'primary_navigation',
-			'menu_class' => 'navbar-nav',
+			'depth' => 2,
+			'container' => 'div',
+			'container_class' => 'collapse navbar-collapse justify-content-end',
+			'menu_class' => 'nav navbar-collapse americanCaptain align-items-end',
 			'walker' => new \App\wp_bootstrap4_navwalker(),
 
 		);
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,14 @@
-<header class="banner">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-  		{!! wp_nav_menu($primarymenu) !!}
-	  @endif
-    </nav>
-</header>
+<nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="{{ home_url('/') }}">
+    <img src="@asset('images/logo.png')" alt="Texas Movie Cars - Logo" class="navLogo">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+{{--     <i class="fas fa-bars"></i> --}}
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    @if (has_nav_menu('primary_navigation'))
+      {!! wp_nav_menu($primarymenu) !!}
+    @endif
+  </div>
+</nav>
